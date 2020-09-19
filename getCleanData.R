c6c16965eb4bef0d8fde277c4c6467ad497fb178
Vasco Alberto Filipe Ribeiro <vascoribeiro.sintra@gmail.com>
2020-09-19 11:48:02 +0100

    Added code for random selection of data from the english twitter source

--- getCleanData.R
+++ getCleanData.R
@@ -1,6 +1,32 @@
+## Get the data
+
 if (!file.exists("Coursera-SwiftKey.zip")) {
         download.file("https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/Coursera-SwiftKey.zip",
               "Coursera-SwiftKey.zip")
         unzip("Coursera-SwiftKey.zip")
 }
+
+## Tokenization and Profanity filtering
+
+### Random selection of data
+set.seed(123)
+lineSelection <- rbinom(10000,1,.5)
+
+con <- file(paste(getwd(), "/final/en_US/en_US.twitter.txt", sep=""), 
+            "r")
+
+enDataSubset <- integer()
+
+for (i in 1:length(lineSelection)){
+        tmp <- readLines(con, lineSelection[i], skipNul = TRUE)
+        enDataSubset <- c(enDataSubset, tmp)
+}
+
+close(con)
+
+rm(tmp, lineSelection, i, con) ## clean unused variables
+
+
+
+
 
