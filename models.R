4761415e690ec0d63a244a7a4b9ef2ea0143763e
Vasco Alberto Filipe Ribeiro <vascoribeiro.sintra@gmail.com>
2020-09-25 17:15:18 +0100

    a plan of action for the ngram model

--- models.R
+++ models.R
@@ -1,1 +1,19 @@
 # Topic models based on the topicmodels R Package
+## This can be interesting to predict more accurately the next words (based on topic being written)
+
+
+
+## Think of this process (more advanced than simple n-gram models, this is a distributed representation):
+## 1 - Data input: "student"
+## 2 - topicmodel determination: "academic"
+## 3 - next word prediction: "papers"
+
+
+
+#To evaluate:
+
+## skip-gram models?
+## n-gram models?
+## ngram model with Kneser-Ney smoothing? -> cmscu library
+## text2vec package?
+## cmscu package?
