6fb5e818960247a71065f2d44482ef8917634b06
Vasco Alberto Filipe Ribeiro <vascoribeiro.sintra@gmail.com>
2020-12-05 11:58:24 +0000

    addind codes for evaluation of input cleaned (with and without stop words), add the ngram function

--- tests/finalTest.R
+++ tests/finalTest.R
@@ -4,7 +4,7 @@ source("getCleanData_fun.R")
 withSW <- read_csv("twitter_subset.csv")
 withoutSW <- read_csv("withoutSW.csv")
 
-userInput <- "my bad"
+userInput <- "my best friend is sad, but I'll helping him out"
 
 evaluateCurses <- function(userInput) {
         userInput <- unlist(str_split(userInput, " "))
@@ -39,32 +39,64 @@ cleanInNoStop <- function(userInput) {
                 trimws() %>%
                 str_split(pattern = " ") %>%
                 unlist()
-
-        #userInputOut <- userInputOut[-grep("^$", userInputOut)]
         
         return(userInputOut)
 }
 
-userInputOut <- cleanInNoStop(userInput)
+userInputClean <- cleanInNoStop(userInput)
 
-cleanInput <- function(userInput) {
-        userInputOut <- userInput %>%
-                replace_contraction() %>%
-                removePunctuation() %>%
-                replace_money() %>%
-                replace_emoticon() %>%
-                replace_symbol() %>%
-                replace_word_elongation() %>%
-                replace_ordinal() %>%
-                replace_number() %>%
-                stripWhitespace() %>%
-                tolower() %>%
-                str_split(pattern = " ") %>%
-                unlist()
+
+if (sum(count_words(userInputClean)) == 0){
+        cleanInput <- function(userInput) {
+                userInputOut <- userInput %>%
+                        replace_contraction() %>%
+                        removePunctuation() %>%
+                        replace_money() %>%
+                        replace_emoticon() %>%
+                        replace_symbol() %>%
+                        replace_word_elongation() %>%
+                        replace_ordinal() %>%
+                        replace_number() %>%
+                        stripWhitespace() %>%
+                        tolower() %>%
+                        str_split(pattern = " ") %>%
+                        unlist()
+                
+                return(userInputOut)
+        }
         
-        return(userInputOut)
+        userInputCleanStop <- cleanInput(userInput)
+        
 }
 
-userInputWith <- cleanInput(userInput)
+## Generate ngrams with and without stop words (may whant to separate this to another file and only make a call here)
+
+### Function
+ngram_freq <- function(x, n) {
+        if (n >= 1) {
+                n_gram <- as_tibble(get.phrasetable(ngram(x[count_words(x) >= n], n)))
+        }else{
+                n_gram <- as_tibble(get.phrasetable(ngram(x, 1)))
+        }
+        n_gram$ngrams <- gsub(" $", "", n_gram$ngrams)
+        return(n_gram)
+}
+
+### Whitout Stop Words - withoutSW
+
+unigram <- ngram_freq(withoutSW$text, 1)
+bigram <- ngram_freq(withoutSW$text, 2)
+trigram <- ngram_freq(withoutSW$text, 3)
+tetragram <- ngram_freq(withoutSW$text, 4)
+pentagram <- ngram_freq(withoutSW$text, 5)
 
 
+## Evaluate against the ngrams
+
+if (exists("userInputCleanStop")) {
+        
+        
+}else{
+        
+        
+}
