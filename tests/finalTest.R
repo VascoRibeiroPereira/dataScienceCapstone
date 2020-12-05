3e55154e6dfb2236e35a2827b01827c12a55a0fe
Vasco Alberto Filipe Ribeiro <vascoribeiro.sintra@gmail.com>
2020-12-05 14:12:18 +0000

    made a code for 4 terms search without stop words

--- tests/finalTest.R
+++ tests/finalTest.R
@@ -4,7 +4,7 @@ source("getCleanData_fun.R")
 withSW <- read_csv("twitter_subset.csv")
 withoutSW <- read_csv("withoutSW.csv")
 
-userInput <- "my best friend is sad, but I'll helping him out"
+userInput <- "my best friend is sad, but I'll helping him" #out" - missing word
 
 evaluateCurses <- function(userInput) {
         userInput <- unlist(str_split(userInput, " "))
@@ -90,8 +90,62 @@ trigram <- ngram_freq(withoutSW$text, 3)
 tetragram <- ngram_freq(withoutSW$text, 4)
 pentagram <- ngram_freq(withoutSW$text, 5)
 
+### Whith Stop Words - withoutSW
+
+unigramSW <- ngram_freq(withSW$text, 1)
+bigramSW <- ngram_freq(withSW$text, 2)
+trigramSW <- ngram_freq(withSW$text, 3)
+tetragramSW <- ngram_freq(withSW$text, 4)
+pentagramSW <- ngram_freq(withSW$text, 5)
+
 
 ## Evaluate against the ngrams
+
+### Guarantee a max five word input and evaluate
+if (length(userInputClean) > 4) {
+        userInput_crop <- userInputClean[(length(userInputClean)-3):length(userInputClean)]
+        userInput_crop <- paste("^", paste(userInput_crop, collapse = " "), sep="")
+} else {
+        userInput_crop <- paste("^", paste(userInputClean, collapse = " "), sep="")
+}
+
+
+if (count_words(userInput_crop) == 4) {
+        
+        freqGramOutput <- tibble()
+        
+        pentagram[grep(userInput_crop, pentagram$ngrams),] ## 4 words search in pentagram
+        
+        userInput_crop <- unlist(str_split(userInput_crop, " "))
+        userInput_crop <- paste("^", paste(userInput_crop[2:length(userInput_crop)], collapse = " "), sep="")
+        
+        freqGramOutput <- rbind(freqGramOutput, tetragram[grep(userInput_crop, tetragram$ngrams),][1:2,]) ## 3 words search in tetragram
+        
+        userInput_crop <- unlist(str_split(userInput_crop, " "))
+        userInput_crop <- paste("^", paste(userInput_crop[2:length(userInput_crop)], collapse = " "), sep="")
+        
+        freqGramOutput <- rbind(freqGramOutput, trigram[grep(userInput_crop, trigram$ngrams),][1:2,]) ## 2 words search in trigram
+        
+        userInput_crop <- unlist(str_split(userInput_crop, " "))
+        userInput_crop <- paste("^", paste(userInput_crop[2:length(userInput_crop)], collapse = " "), sep="")
+        
+        freqGramOutput <- rbind(freqGramOutput, bigram[grep(userInput_crop, bigram$ngrams),][1:2,]) ## 1 word search in bigram
+        
+}
+
+
+
+
+
+
+
+
+
+
+### Number of Terms
+
+
+
 
 if (exists("userInputCleanStop")) {
         
